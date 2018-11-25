32b32e036ebe5b67b6dfd8ca5f920a04f2bb3aca
Spankie Dee <odohi.david@gmail.com>
2018-11-25 18:35:15 +0100

    remove mysql implementation of migrations

--- migrations/table_migrations.go
+++ migrations/table_migrations.go
@@ -2,12 +2,10 @@ package migrations
 
 import (
 	"github.com/ChrisPowellIinc/allofusserver/internal/config"
-	"github.com/ChrisPowellIinc/allofusserver/models"
 )
 
 // MakeMigrations will create and modify existing tables without deleting existing ones
 func MakeMigrations(con *config.Config) {
-	// con := config.GetConf(false, false)
-	db := con.DB
-	db.AutoMigrate(&models.User{})
+	//  Do migrations here pplease...
+
 }
