5d00b49a4f5637431b9db0a644363a16cf467fbc
Spankie Dee <odohi.david@gmail.com>
2018-12-06 07:01:30 +0100

    create new src folder to hold the javascripts

---
+++ src/pages/404/index.js
@@ -0,0 +1,10 @@
+import m from "mithril";
+import "./style.scss";
+
+let _404 = {
+  view: () => (
+    <div> 404 Page </div>
+  )
+};
+
+export default _404;
---
+++ src/pages/landing/index.js
@@ -0,0 +1,10 @@
+import m from "mithril";
+import "./style.scss";
+
+let Landing = {
+  view: () => (
+    <div className = "text-primary"> Landing Page </div>
+  )
+};
+
+export default Landing;
