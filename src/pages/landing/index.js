66e3e1b871c8c1ceb5c8243d4c7292a22c329813
Spankie Dee <odohi.david@gmail.com>
2018-12-06 18:17:22 +0100

    add cover page css

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,7 +1,51 @@
 import m from "mithril";
+import "css/cover.css";
 
 const Landing = {
-  view: () => <div class="text-primary">Landing Pages</div>
+  view: () => (
+    <div class="landing text-center">
+      <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="masthead mb-auto">
+          <div class="inner">
+            <h3 class="masthead-brand">All of Us</h3>
+            <nav class="nav nav-masthead justify-content-center">
+              <a class="nav-link active" href="#">
+                Home
+              </a>
+              <a class="nav-link" href="#">
+                Features
+              </a>
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </nav>
+          </div>
+        </header>
+
+        <main role="main" class="inner cover">
+          <h1 class="cover-heading">All of Us.</h1>
+          <p class="lead">All of us is a social media network for ...</p>
+          <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary mr-1">
+              Login
+            </a>
+            <a href="#" class="btn btn-lg btn-secondary ml-1">
+              Signup
+            </a>
+          </p>
+        </main>
+
+        <footer class="mastfoot mt-auto">
+          <div class="inner">
+            <p>
+              &copy 2018 <a href="https://getbootstrap.com/">allofus</a>, by{" "}
+              <a href="https://twitter.com/mdo">allofus</a>.
+            </p>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
 };
 
 export default Landing;
