320b58b216ad3bdaae9c365e49b8ed661afb6897
Spankie Dee <odohi.david@gmail.com>
2018-12-14 20:45:22 +0100

    add profile route resolver and serve 404 page.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "izitoast/dist/css/iziToast.min.css";
 
 // Pages
+import _404 from "pages/404";
 import Landing from "pages/landing";
 import Register from "pages/auth/register";
 import Login from "pages/auth/login";
+
+import Profile from "pages/profile";
 
 const root = document.getElementById("app");
 
@@ -18,12 +21,14 @@ m.route(root, "/", {
   "/": Landing,
   "/register": Register,
   "/login": Login,
+  "/profile": {
+    onmatch: (args, requestedPath) => {
+      console.log("I am going to check out login auth here..");
+      return new Promise(resolve => resolve(Profile));
+    }
+  },
   // Lazy load 404 page, use this method to lazy load other pages
   "/:404": {
-    onmatch: () =>
-      new Promise(resolve => {
-        console.log("what was you thinking...");
-        return resolve;
-      })
+    onmatch: (args, requestedPath) => new Promise(resolve => resolve(_404))
   }
 });
