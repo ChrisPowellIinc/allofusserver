8ce8de1405ac2ccbc1389ee2333d57fd4c36d41f
Spankie Dee <odohi.david@gmail.com>
2018-12-14 20:46:08 +0100

    profile page boilerplate

---
+++ src/pages/profile/index.js
@@ -0,0 +1,14 @@
+import m from "mithril";
+
+const Profile = {
+  view: vnode => (
+    <section>
+      <div>
+        <h1>My Profile</h1>
+      </div>
+      <small>This is where my profile dashboard goes...</small>
+    </section>
+  )
+};
+
+export default Profile;
