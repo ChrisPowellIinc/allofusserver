4b6fb07aa6db6ba862c4cf4e57c3e0c395fc7390
Spankie Dee <odohi.david@gmail.com>
2018-12-14 20:45:55 +0100

    create 404 page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,7 +1,19 @@
 import m from "mithril";
+import "css/404.css";
 
 const _404 = {
-  view: () => <div>404 Page</div>
+  view: () => (
+    <section class="error_section">
+      <p class="error_section_subtitle">Opps Page is not available !</p>
+      <h1 class="error_title">
+        <p>404</p>
+        404
+      </h1>
+      <a href="/" class="btn btn-primary" oncreate={m.route.link}>
+        Back to home
+      </a>
+    </section>
+  )
 };
 
 export default _404;
