57242a6fcd170d1acf381b7dc4faff6b0e9f62df
Spankie Dee <odohi.david@gmail.com>
2019-02-07 23:31:09 +0100

    add a response handler for reuse by route hadlers

--- models/messages.go
+++ models/messages.go
@@ -1,7 +1,21 @@
 package models
 
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
 type Response struct {
 	Message string                 `json:"message"`
 	Status  int                    `json:"status"`
 	Data    map[string]interface{} `json:"data"`
+}
+
+func HandleResponse(w http.ResponseWriter, r *http.Request, message string, status int) {
+	res := Response{}
+	res.Message = message
+	res.Status = status
+	render.Status(r, status)
+	render.JSON(w, r, res)
 }
