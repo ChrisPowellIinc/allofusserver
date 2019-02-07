3172b53719aff82488f53cb6615ed7a24f160856
Spankie Dee <odohi.david@gmail.com>
2019-02-07 23:33:41 +0100

    go fmt user handlers

--- modules/user/handlers.go
+++ modules/user/handlers.go
@@ -1,22 +1,19 @@
 package user
 
 import (
-	"mime/multipart"
 	"bytes"
-	"crypto/rand"
-	"encoding/hex"
 	"log"
+	"mime/multipart"
 	"net/http"
-	"os"
 	"path/filepath"
-	
+
+	"github.com/ChrisPowellIinc/allofusserver/internal/config"
 	"github.com/ChrisPowellIinc/allofusserver/models"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/globalsign/mgo/bson"
 	"github.com/go-chi/render"
-	"github.com/ChrisPowellIinc/allofusserver/internal/config"
-	"github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
 )
 
 // Get : Shows that the app is working
@@ -36,29 +33,28 @@ func (handler *Handler) Get(w http.ResponseWriter, r *http.Request) {
 }
 
 func uploadFileToS3(s *session.Session, file multipart.File, fileName string, size int64, con *config.Config) error {
-    // get the file size and read
-    // the file content into a buffer
+	// get the file size and read
+	// the file content into a buffer
 	buffer := make([]byte, size)
 	file.Read(buffer)
-	
 
-    // config settings: this is where you choose the bucket,
-    // filename, content-type and storage class of the file
-    // you're uploading
-    e, s3err := s3.New(s).PutObject(&s3.PutObjectInput{
-        Bucket:               aws.String(con.Constants.S3Bucket),
-        Key:                  aws.String(fileName),
-        ACL:                  aws.String("public-read"),
-        Body:                 bytes.NewReader(buffer),
-        ContentLength:        aws.Int64(int64(size)),
-        ContentType:          aws.String(http.DetectContentType(buffer)),
-        ContentDisposition:   aws.String("attachment"),
-        ServerSideEncryption: aws.String("AES256"),
-        StorageClass:         aws.String("INTELLIGENT_TIERING"),
-    })
+	// config settings: this is where you choose the bucket,
+	// filename, content-type and storage class of the file
+	// you're uploading
+	e, s3err := s3.New(s).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(con.Constants.S3Bucket),
+		Key:                  aws.String(fileName),
+		ACL:                  aws.String("public-read"),
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(int64(size)),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+		StorageClass:         aws.String("INTELLIGENT_TIERING"),
+	})
 	log.Println(e)
 	log.Println(s3err)
-    return s3err
+	return s3err
 }
 
 // UploadProfilePic uploads a user's profile picture
