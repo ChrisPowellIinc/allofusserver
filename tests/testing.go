e87a40beb54c504a597c2a7df47ee962c9f5ba75
Spankie Dee <odohi.david@gmail.com>
2019-02-10 00:14:28 +0100

    add getAuth token function to retrieve a auth token for the tests

--- tests/testing.go
+++ tests/testing.go
@@ -1,7 +1,12 @@
 package tests
 
 import (
+	"log"
+
+	"github.com/ChrisPowellIinc/allofusserver/internal/jwt"
+
 	"github.com/ChrisPowellIinc/allofusserver/internal/config"
+	jwtgo "github.com/dgrijalva/jwt-go"
 )
 
 var Con *config.Config
@@ -13,4 +18,15 @@ func init() {
 // GetConfig returns a testing config to be used by every tests
 func GetConfig() *config.Config {
 	return config.GetConf(true, false)
+}
+
+func GetAuth() string {
+	jwt.Register([]byte(Con.Constants.JWTSecret))
+	token := jwtgo.NewWithClaims(jwtgo.SigningMethodHS256, jwtgo.MapClaims{"user_email": "odohi.david@gmail.com"})
+	tokenString, err := token.SignedString([]byte(Con.Constants.JWTSecret))
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return tokenString
 }
